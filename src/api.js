9b769d8b5bc1a15ddeca91dab6f580747fa62c3d
Tomar <prajwaltomar@kpmg.com>
2024-06-05 12:17:54 +0530

    Added weather app at /weather

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "7f414f9cf2mshf4776f4b4150e70p197796jsn8fac0af4cdd2",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "2a1be2fd5b32c12d9c929f9cb82100ba";
